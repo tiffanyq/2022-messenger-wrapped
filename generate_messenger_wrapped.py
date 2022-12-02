25079ca7c8bf8460d5aa692afd3d4f7a07845d2f
Tiffany Quon <tiffany.quon@alumni.ubc.ca>
2022-12-01 21:02:57 -0800

    2022 messenger wrapped

---
+++ generate_messenger_wrapped.py
@@ -0,0 +1,146 @@
+"""
+Run this script after downloading Messenger data from
+January 1 2020 - December 1 2020 (or some later date in 2020)
+in JSON format. Put the `messages` folder in the `2020messengerwrapped`
+directory.
+
+In this directory: run `python generate_messenger_wrapped.py`
+to generate the file that index.html will use to generate
+your 2020 Messenger Wrapped!
+"""
+
+import json
+import os
+import string
+
+MESSENGER_DIR = './messages/inbox'
+OUTPUT_FILE_DIR = './js'
+OUTPUT_FILENAME = 'your_messenger_wrapped.js'
+NUM_TOP_PEOPLE_AND_PHRASES = 5
+NUM_PEOPLE_TO_PRINT = 25
+NUM_PHRASES_TO_PRINT = 100
+include_group_chats = True
+total_messages = 0
+total_reacts_and_stickers = 0
+people = {}
+phrases = {}
+top_people = []
+top_phrases = []
+phrase_length = 4 # default: 4
+
+if __name__ == "__main__":
+  print('\nYOUR NAME')
+  your_name = input('Please input your name as shown on Messenger (so your top person is not you in the generated data!). Capitalization and whitespace matter:\n').strip()
+  # omit certain senders upon request.
+  print('\nSENDERS TO OMIT')
+  print('(Press ENTER if there are no senders you wish to omit from your list of top senders.)')
+  senders_to_omit = input('Are there any senders you would like to omit from your list of top senders? \n Please separate phrases with commas. Capitalization and whitespace matter: e.g. Person One, Person Two, Person Three\n')
+  senders_to_omit = [] if not senders_to_omit else [p.strip() for p in senders_to_omit.split(',')]
+  # include/exclude group chats
+  print('\nGROUP CHATS')
+  print('(Press ENTER to default to including group chat messages.)')
+  include_group_chats = input('Would you like to include messages sent in group chats when determining who sent you the most messages this year? (yes/no)\n').lower().strip()
+  include_group_chats = True if include_group_chats == 'yes' or include_group_chats == 'y' else False
+  # determine phrase length to search for
+  print('\nPHRASE LENGTH')
+  print('(Press ENTER to default to 4-word phrases.)')
+  temp_phrase_length = input('How many words should your most frequent phrases contain? Enter 3, 4, 5, or 6 (default is 4):\n')
+  if temp_phrase_length and int(temp_phrase_length) in {3,4,5,6}:
+    phrase_length = int(temp_phrase_length)
+  # omit certain phrases upon request. If an ignorable phrase is found in a message, the whole message will be ignored.
+  print('\nPHRASES TO OMIT')
+  print('(Press ENTER if there are no phrases you wish to omit from your list of most-used phrases.)')
+  phrases_to_omit = input('Are there any phrases you would like to omit from your top phrases of the year? If any of these phrases are found in a message, the whole message will be ignored for the purpose of identifying top phrases.\n Please ignore punctuation (parentheses, apostrophes, etc) and separate phrases with commas, e.g. Words with Friends, etc\n')
+  phrases_to_omit = [] if not phrases_to_omit else [p.strip() for p in phrases_to_omit.lower().split(',')]
+  phrases_to_omit.extend(['The video chat ended', 'missed your call', 'info like active status'])
+
+  for dir, _, files in os.walk(MESSENGER_DIR):
+    for file in files:
+      if file == 'message_1.json':
+        with open(os.path.join(dir, file)) as json_file:
+          data = json.load(json_file)
+          is_group_chat = len(data['participants']) > 2
+          # Track number of messages sent by different people
+          for m in data['messages']:
+            sender_name = m['sender_name']
+            # only count messages towards top senders if it satisfies user's preference
+            if include_group_chats or not is_group_chat:
+              if sender_name in people:
+                people[sender_name] = people[sender_name] + 1
+              else:
+                people[sender_name] = 1
+
+            # Track reacts that you give
+            if 'reactions' in m:
+              for react in m['reactions']:
+                if react['actor'] == your_name:
+                  total_reacts_and_stickers = total_reacts_and_stickers + 1
+
+            # Also track stickers that you send
+            if 'sticker' in m and sender_name == your_name:
+              total_reacts_and_stickers = total_reacts_and_stickers + 1
+
+            # Extract 5-word phrases from your messages
+            if 'content' in m and m['sender_name'] == your_name:
+              s = m['content'].encode('latin1').decode('utf8').lower().translate(str.maketrans('', '', string.punctuation))
+              ignore_phrase = False
+              for ignorable_phrase in phrases_to_omit:
+                if ignorable_phrase in s:
+                  ignore_phrase = True
+                  break
+              if not ignore_phrase:
+                words = s.split()
+                num_words = len(words)
+                if num_words >= phrase_length:
+                  for i in range(0, num_words-phrase_length):
+                    phrase = " ".join(words[i:phrase_length+i])
+                    if phrase in phrases:
+                      phrases[phrase] = phrases[phrase] + 1
+                    else:
+                      phrases[phrase] = 1
+
+  # Generate top list of people
+  # first, identify how many messages you sent
+  # remove yourself from the list
+  if your_name in people:
+    total_messages = people[your_name]
+  people.pop(your_name, None)
+
+  # remove ignored senders from the list
+  for sender_name in senders_to_omit:
+    people.pop(sender_name, None)
+
+  people = sorted(people.items(), key=lambda x: x[1], reverse=True)
+
+  print('\nVIEW TOP MESSAGE SENDERS')
+  print_people = input('Would you like to view the names and number of messages sent by the top 25 people who sent you the most messages this year? (yes/no)\n').lower().strip()
+  if print_people == 'yes' or print_people == 'y':
+    for i in range(0, min(len(people), NUM_PEOPLE_TO_PRINT)):
+      print(people[i])
+
+  print('\nSHARE NAMES OR NUMBER OF MESSAGES')
+  print('(Press ENTER to default to printing the NUMBER OF MESSAGES SENT BY instead of the NAMES OF your top 5 people.)')
+  print('Would you like to display the names or the number of messages that your top 5 people sent?')
+  share_names = input('Type "names" (with no quotation marks) for names; type anything else to display the number of messages instead:\n').lower()
+  top_people = [p[0] for p in people[:min(len(people), NUM_TOP_PEOPLE_AND_PHRASES)]] if share_names == 'names' else [p[1] for p in people[:min(len(people), NUM_TOP_PEOPLE_AND_PHRASES)]]
+
+  phrases = sorted(phrases.items(), key=lambda x: x[1], reverse=True)
+  print('\nVIEW MOST COMMON PHRASES')
+  print_phrases = input('Would you like to view your top 100 most frequently used phrases? (yes/no)\n').lower().strip()
+  if print_phrases == 'yes' or print_phrases == 'y': 
+    for i in range(0, min(len(phrases), NUM_PHRASES_TO_PRINT)):
+      print(phrases[i])
+  
+  top_phrases = [p[0] for p in phrases[:min(len(phrases), NUM_TOP_PEOPLE_AND_PHRASES)]]
+
+  output_data = {
+    "total_messages": total_messages,
+    "total_reacts_and_stickers": total_reacts_and_stickers,
+    "share_names": share_names == 'names',
+    "top_people": top_people,
+    "top_phrases": top_phrases,
+  }
+
+  with open(os.path.join(OUTPUT_FILE_DIR, OUTPUT_FILENAME), 'w') as outfile:
+    outfile.write("yourData=")
+    json.dump(output_data, outfile)
---
+++ js/sketch.js
@@ -0,0 +1,186 @@
+const SHAPES = ["circle","square", "triangle"];
+const COLORS = ["orange", "pink", "green", "yellow", "black", "purple"];
+const NUM_SHAPE_LAYERS = getRandom(4,6);
+const MIN_OBJ = 4;
+const MAX_OBJ = 20;
+const PFP_SIZE = 235;
+const FRAME_RATE = 4;
+const HOLD_LENGTH = FRAME_RATE * 8;
+let pContent;
+let currLayerNumber = 0;
+let holdCount = 1;
+let numLayersToDraw = 0;
+let countingUp = true;
+let holding = false;
+
+class ShapeBackground {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+    this.colors = this.getColors();
+    this.numObj = this.getNumObj();
+    this.shapes = this.getShapes();
+    this.sizes = this.getSizes();
+    this.radii = this.getRadii();
+  }
+
+  reset() {
+    this.colors = this.getColors();
+    this.numObj = this.getNumObj();
+    this.shapes = this.getShapes();
+    this.sizes = this.getSizes();
+    this.radii = this.getRadii();
+  }
+
+  getColors() {
+    let lastColor = "";
+    let localColors = [];
+    for (let i = 0; i < NUM_SHAPE_LAYERS; i++) {
+      let tempIdx = Math.floor(getRandom(0,5));
+      let tempColor = COLORS[tempIdx];
+      while (tempColor === lastColor) {
+        tempColor = COLORS[Math.floor(getRandom(0,5))];
+      }
+      localColors.push(tempColor);
+      lastColor = tempColor;
+    }
+    return localColors;
+  }
+
+  getNumObj() {
+    let localNumObj = [];
+    for (let i = 0; i < NUM_SHAPE_LAYERS; i++) {
+      localNumObj.push(Math.floor(getRandom(MIN_OBJ, MAX_OBJ + 1)));
+    }
+    return localNumObj;
+  }
+
+  getShapes() {
+    let lastShape = "";
+    let localShapes = [];
+    for (let i = 0; i < NUM_SHAPE_LAYERS; i++) {
+      let tempShape = SHAPES[Math.floor(getRandom(0,SHAPES.length-1))];
+
+      localShapes.push(tempShape);
+      lastShape = tempShape;
+    }
+    return localShapes;
+  }
+
+  getRadii() {
+    let localRadii = [];
+    let currRadii = Math.max(this.width, this.height)/2;
+    let currIncrement = 0;
+    for (let i = 0; i < NUM_SHAPE_LAYERS; i++) {
+      localRadii.push(currRadii - currIncrement);
+      currIncrement += (10 + Math.floor(getRandom(0,this.width/12)));
+    }
+    return localRadii;
+  }
+
+  getSizes() {
+    let localSizes = [];
+    for (let i = 0; i < NUM_SHAPE_LAYERS; i++) {
+      localSizes.push(Math.floor(getRandom(this.width/16, this.width/2)));
+    }
+    return localSizes;
+  }
+
+  buildBackground(i) { // i = layer number
+    const c = this.getCurrColor(this.colors[i]);
+    fill(c);
+    if (this.shapes[i] === 'square') {
+      for (let j = 0; j < this.numObj[i]; j++) {
+        push();
+        translate(PFP_SIZE/2, PFP_SIZE/2);
+        rotate(TWO_PI * j / this.numObj[i]);
+        rect(this.radii[i], 0, this.sizes[i], this.sizes[i]);
+        pop();
+      }
+    } else if (this.shapes[i] === 'circle') {
+      for (let j = 0; j < this.numObj[i]; j++) {
+        push();
+        translate(PFP_SIZE/2, PFP_SIZE/2);
+        rotate(TWO_PI * j / this.numObj[i]);
+        circle(this.radii[i], 0, this.sizes[i]);
+        pop();
+      }
+    } else { // triangle
+      for (let j = 0; j < this.numObj[i]; j++) {
+        push();
+        translate(PFP_SIZE/2, PFP_SIZE/2);
+        rotate(TWO_PI * j / this.numObj[i]);
+        triangle(
+          this.radii[i], this.radii[i],
+          0,40,
+          40,10
+        );
+        pop();
+      }
+    }
+  }
+
+  getCurrColor(c) {
+    let nc;
+    if (c === 'orange') {
+      nc = color(229,129,46);
+    } else if (c === 'pink') {
+      nc = color(226,116,186);
+    } else if (c === 'green') {
+      nc = color(89,204,96);
+    } else if (c === 'yellow') {
+      nc = color(245,252,89);
+    } else if (c === 'black') {
+      nc = color(19,20,18);
+    } else { // purple
+      nc = color(84,32,172);
+    }
+    return nc;
+  }
+}
+
+function setup() {
+  pCnv = createCanvas(PFP_SIZE, PFP_SIZE);
+  pCnv.parent(document.getElementById("profile-photo"));
+  rectMode(CENTER);
+  noStroke();
+  pContent = new ShapeBackground(PFP_SIZE, PFP_SIZE);
+  frameRate(FRAME_RATE);
+  background(color(84,32,172));
+  // kick off process
+  pContent.buildBackground(currLayerNumber);
+  currLayerNumber += 1;
+}
+
+function draw() {
+  background(color(84,32,172));
+  for (let i = 0; i < currLayerNumber; i++) {
+    pContent.buildBackground(i);
+  }
+
+  if (countingUp) {
+    if (!holding) {
+      currLayerNumber = Math.min(currLayerNumber + 1, NUM_SHAPE_LAYERS);
+      if (currLayerNumber === NUM_SHAPE_LAYERS) {
+        holding = true;
+      }
+    } else {
+      holdCount += 1;
+      if (holdCount > HOLD_LENGTH) {
+        holdCount = 0;
+        holding = false;
+        countingUp = false;
+      }
+    }
+  } else {
+    currLayerNumber = Math.max(currLayerNumber - 1, 0);
+    if (currLayerNumber === 0) {
+      countingUp = true;
+      pContent.reset();
+    }
+  }
+}
+
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
